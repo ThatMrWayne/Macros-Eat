1807e94f6fd9624958ff6f333cca6f584ba24550
Wayne Hsu <whph60308@gmail.com>
2022-06-11 11:52:43 +0800

    test deploy 6

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd /home/ubuntu
+pip3 install redis==4.2.2
 sudo docker container stop flask-macroseat
 sudo docker container rm flask-macroseat
 sudo docker image rmi 298325099374.dkr.ecr.us-east-1.amazonaws.com/macroseat:latest
-python3 test-redis.py 
+sudo python3 test-redis.py 
 aws ecr get-login-password --region us-east-1 |sudo docker login --username AWS --password-stdin 298325099374.dkr.ecr.us-east-1.amazonaws.com
 sudo docker image pull 298325099374.dkr.ecr.us-east-1.amazonaws.com/macroseat:latest
